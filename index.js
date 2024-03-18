06c7a458867554bed67b135e1e482fded3fac0fe
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-03-18 16:23:35 -0300

    Se configuro el puerto

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ console.log('Hola mundo');
 
 
 // 1. Configurar un puerto 
+const app = express();
+
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'),()=> {
+    console.log('Estoy en el puerto '+ app.get('port'))
+})
+
 
 // 2. Configurar middlewares 
 
+
 // 3. Configuración de las rutas 
+
