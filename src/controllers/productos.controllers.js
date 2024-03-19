3497b852a084b6186b984fe0c05e3bae0caba37c
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-03-19 02:52:40 -0300

    Se agrego la funcion para editar un producto mediante su id

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -44,4 +44,24 @@ export const obtenerProducto = async (req, res) => {
       console.error(error);
       res.status(404).json({ mensaje: "No se encontro el producto buscado" });
     }
-  };
+};
+
+export const editarProducto = async (req, res) => {
+    try {
+      //extraer el id del producto a editar y los datos del producto a editar del req.body
+      //buscar si encontramos el producto con el id
+      const productoBuscado =  await Producto.findById(req.params.id);
+      //no encontre el producto buscado?
+      if(!productoBuscado){
+          //enviar un mensaje de error en caso de no encontrar el producto
+          return res.status(404).json({mensaje: "El producto no fue encontrado."});
+      }
+      //editar el producto
+      await Producto.findByIdAndUpdate(req.params.id, req.body);
+      //contestamos al front con un status 200
+      res.status(200).json({mensaje: "El producto fue editado correctamente"})
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({mensaje: "Ocurrio un error al editar el producto"})
+    }
+};
