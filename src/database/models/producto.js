693296388e560117ec45fd5b687ea7b4f1e1a642
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-03-19 01:04:10 -0300

    Agregue el modelo del Producto

---
+++ src/database/models/producto.js
@@ -0,0 +1,50 @@
+import mongoose, {Schema} from "mongoose";
+
+const productoSchema = new Schema({
+    nombreProducto:{
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 50,
+        unique:true
+    },
+    precio:{
+        type:Number,
+        required:true,
+        min:100,
+        max:10000
+    },
+    imagen:{
+        type:String,
+        required:true,
+        validate:{
+            validator: function(valor){
+                //validar el valor con un patron
+                return /^(http|https):\/\/\S+\.(jpeg|jpg|gif|png|bmp)$/i.test(valor)
+            },
+            message: props => `${props.value} no es una url de imagen valida.`
+        }
+    },
+    categoria:{
+        type:String,
+        required:true,
+        enum:['Bebidas Calientes','Bebidas Frias','Pastelería y dulces','Desayunos y Snacks']
+    },
+    descripcionBreve:{
+        type:String,
+        required:true,
+        minLength: 10,
+        maxLength: 70,
+    },
+    descripcionAmplia:{
+        type:String,
+        required:true,
+        minLength: 15,
+        maxLength: 800,
+    }
+})
+
+//vamos a generar el modelo Producto
+const Producto = mongoose.model('producto', productoSchema)
+
+export default Producto;
